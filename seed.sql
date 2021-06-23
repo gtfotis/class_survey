c85dfb287e114e20db112555a0193a1adae6bcfc
otis wilcox <otiswilcox@gmail.com>
2021-06-23 10:36:38 -0400

    pushing up my sql class survey

---
+++ seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO rankings
+    (ranking_title)
+VALUES
+    ('AMAZING'),
+    ('GREAT'),
+    ('Alright..'),
+    ('Meh'),
+    ('Poopy'),
+    ('What am I doing..');
+
+INSERT INTO topics
+    (topic_name, topic_score)
+VALUES
+    ('HTML', 6),
+    ('CSS', 6),
+    ('JavaScript', 6),
+    ('PostgreSQL', 6),
+    ('Node', 6),
+    ('Express', 6);
